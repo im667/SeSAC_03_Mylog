d4ea84d186d87df338bd41c364132c2f990f5e5d
kwon <koh9037@gmail.com>
2021-10-12 09:13:08 +0900

    Initial Commit

---
+++ SeSAC_03_Mylog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_03_Mylog
+//
+//  Created by mac on 2021/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
